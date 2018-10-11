d028fd45456011927b6c4266bdbb7fb0c9fd30d2
Evgeny Ivanov <busforward@gmail.com>
2018-10-11 23:29:01 +0500

    Main chert finsh

--- js/function.js
+++ js/function.js
@@ -42,8 +42,6 @@ $(document).ready(function() {
         return false;
     });
 
-    checkOnResize();
-
     $('.topSlider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -101,6 +99,31 @@ $(document).ready(function() {
 
     matchHeightcatalogueItem();
 
+    $('.productFilter__drop').on('click', function(event) {
+        event.preventDefault();
+        var self = $(this);
+        var drop = $(this).closest('.productFilter__item').find('.productDrop');
+        if (self.hasClass('open')) {
+            self.removeClass('open');
+            drop.removeClass('open');
+        } else {
+            self.addClass('open');
+            drop.addClass('open');
+        }
+        $('.productDrop').not(drop).removeClass('open');
+        $('.productFilter__drop').not(self).removeClass('open');
+    });
+
+    mobileNav();
+
+    counter();
+
+    checkOnResize();
+
+    if (isXsWidth()) {
+        stiky();
+    }
+
 });
 
 $(window).resize(function(event) {
@@ -129,11 +152,15 @@ function checkOnResize() {
         $('.footer__center').insertBefore('.footer__left');
         $('.shopItem__title').prependTo('.shopItem__top');
         $('.shopItem__sale').prependTo('.shopItem__description');
+
+        
+
     } else {
         $('.footer__center').insertAfter('.footer__right');
         $('.shopItem__title').prependTo('.shopItem__info');
         $('.shopItem__sale').prependTo('.shopItem__grid > div:last');
     }
+    filterReplace();
 }
 
 function gridMatch() {
@@ -142,12 +169,85 @@ function gridMatch() {
    	});
 }
 
-function fontResize() {
-    var windowWidth = $(window).width();
-    if (windowWidth >= 1200) {
-    	var fontSize = windowWidth/19.05;
-    } else if (windowWidth < 1200) {
-    	var fontSize = 60;
+function filterReplace() {
+    var filtreItem = $('.productFilter__item_mob');
+    var filterWrap = $('.productFilter');
+    var filterWrapMob = $('.mobileNav__filter');
+    if (isXsWidth()) {
+        filtreItem.appendTo(filterWrapMob);
+    } else {
+        filtreItem.prependTo(filterWrap);
     }
-	$('body').css('fontSize', fontSize + '%');
 }
+
+function mobileNav() {
+    var toggle = $('.navbar__toggle, .js_show_nav'),
+        nav = $('.mobileNav'),
+        body = $('body');
+
+    toggle.on('click', function(event) {
+        event.preventDefault();
+        console.log('click')
+        if (toggle.hasClass('open')) {
+            body.removeClass('navbar-open');
+            nav.removeClass('open');
+            toggle.removeClass('open');
+        } else {
+            body.addClass('navbar-open');
+            nav.addClass('open');
+            toggle.addClass('open');
+        }
+    });
+}
+
+function counter() {
+    $('.js_counter .minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.js_counter .plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+}
+
+function stiky() {
+    // Stiky menu // Липкое меню. При прокрутке к элементу #header добавляется класс .stiky который и стилизуем
+    var head = $('.header');
+    if (!$('.header').hasClass('header_contrast')) {
+        var HeaderTop = head.offset().top;
+        if (isXsWidth()) {
+            $(window).scroll(function(){
+                if( $(window).scrollTop() > HeaderTop ) {
+                        head.addClass('stiky');
+                } else {
+                        head.removeClass('stiky');
+                }
+            });
+        }
+    }
+    $("[data-scroll-to-fixed]").each(function() {
+        var offsetHeight = $(window).height()-$(this).height();
+        $(this).stick_in_parent({
+            parent: $('.main__content'),
+            // recalc_every: true,
+            offset_top: offsetHeight
+        });
+    })
+}
+
+// function fontResize() {
+//     var windowWidth = $(window).width();
+//     if (windowWidth >= 1200) {
+//     	var fontSize = windowWidth/19.05;
+//     } else if (windowWidth < 1200) {
+//     	var fontSize = 60;
+//     }
+// 	$('body').css('fontSize', fontSize + '%');
+// }
